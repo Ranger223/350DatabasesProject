84d3eff4636408d9598b632eb690cb724bd887f0
Ranger223 <112410840+Ranger223@users.noreply.github.com>
2024-02-29 13:44:33 -0700

    Update FactionsandUsersSQL.sql with FACTIONCONTROLS insert script
    committed by Steven Bates

--- FactionsandUsersSQL.sql
+++ FactionsandUsersSQL.sql
@@ -26,3 +26,49 @@ INSERT INTO [dbo].[DBUSER]
            ('vendingmachineoflove', 'sodapop@spacegmail.com', 1234, 1, 8),
            ('werewolfvampire', 'darkfall@spacegmail.com', 1234, 3, NULL)
 GO
+
+USE [Milestone3]
+GO
+SET IDENTITY_INSERT FACTIONCONTROLS ON
+INSERT INTO [dbo].[FACTIONCONTROLS]
+           ([FacID],[CBID])
+     VALUES
+           (1, 58),
+	(1, 12),
+           (1, 60),
+	(1, 63),
+	(1, 68),
+	(2, 1),
+	(2, 2),
+	(2, 3),
+	(2, 4),
+	(2, 5),
+	(2, 6),
+	(2, 7),
+	(2, 8),
+	(2, 9),
+	(2, 10),
+	(4, 43),
+	(4, 47),
+	(5, 12),
+	(5, 31),
+	(5, 37),
+	(6, 11),
+	(6, 12),
+	(6, 13),
+	(6, 14),
+	(7, 26),
+	(7, 28),
+	(7, 29),
+	(8, 40),
+	(8, 41),
+	(8, 42),
+	(9, 48),
+	(9, 49),
+	(10, 33),
+	(10, 34),
+	(10, 35)
+GO
+
+
+
